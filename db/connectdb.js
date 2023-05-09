71a4e253c720c08e877703bdf53b1fa097d3808c
ritikraghuwanshi <ritikraghuwanshi987@gmail.com>
2023-05-09 14:06:08 +0530

    hello ritik

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,9 +1,10 @@
 const mongoose  =require('mongoose')
-const url = "mongodb://127.0.0.1:27017/Api"
+// const url = "mongodb://127.0.0.1:27017/Api"
+const live_Url = "mongodb+srv://ritikraghuwanshi728:ritikraghuwanshi@cluster0.oy5gife.mongodb.net/blogapi"
 
 
 const connectdb =()=>{
-    return mongoose.connect(url)
+    return mongoose.connect(live_Url)
 
     .then(()=>{
         console.log('Database connected..')
